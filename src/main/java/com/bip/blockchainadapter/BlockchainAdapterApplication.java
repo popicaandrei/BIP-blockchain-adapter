ee71ecda92cebd13647d00235b7800bf6dfa6292
Andrei Popica <popica.andreivlad@gmail.com>
2022-05-20 10:31:31 +0300

    Setup commit

---
+++ src/main/java/com/bip/blockchainadapter/BlockchainAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.bip.blockchainadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bip/blockchainadapter/BlockchainadapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bip.blockchainadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainadapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
